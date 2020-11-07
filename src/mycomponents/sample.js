84beb83cc112d6d256ffc6c85842227494b305c0
Shorojit1997 <shorojitsarkar1997@gmail.com>
2020-11-08 00:45:38 +0600

    add my components file

---
+++ src/mycomponents/sample.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class sample extends Component {
+    render() {
+        return (
+            <div>
+                <h2>My name is sagor sarkar</h2>
+            </div>
+        );
+    }
+}
+
+export default sample;
